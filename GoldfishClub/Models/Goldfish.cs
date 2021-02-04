8a277071fe5fab01c5b74435e8f03149f8d83cd9
Truc Chau <tc9538@cs.ship.edu>
2021-02-04 12:17:58 -0500

    update project

---
+++ GoldfishClub/Models/Goldfish.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoldfishClub.Models
+{
+    public class Goldfish
+    {
+        public const double AmoniaNumber= 0;
+        public const double NitriteNumber = 0;
+
+        public const double NitrateLowEndNum = 0;
+        public const double NitrateHighEndNum = 50;
+
+        public int GoldfishId { get; set; }
+        public string Name { get; }
+        public string Color { get; }
+        public string CountryOfOrigin { get;}
+
+        public List<string> food = new List<string> { "spinach", "egg", "cucumber", "broccoli", "pea"};
+        public int TankSize { get; }
+        public int Temperature { get; }
+
+        public double AmoniaLevel { get; }
+        public double NitrateLevel { get; }
+        public double NitriteLevel { get; }
+
+        public double PHLevel { get; }
+
+        
+        
+        
+
+
+        
+    }
+}
---
+++ GoldfishClub/Models/IGoldfishRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoldfishClub.Models
+{
+    public interface IGoldfishRepository
+    {
+        IEnumerable<Goldfish> AllGoldfish { get; }
+        Goldfish GetGoldfishById(int goldfishId);
+    }
+}
